e7a5fd1ae128b2967721c2889f62a78815d68ee7
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2025-01-30 02:38:24 +0100

    Fully tutorialized SSBO

--- gpu_rendering/shaders/compute/main_ssbo.py
+++ gpu_rendering/shaders/compute/main_ssbo.py
@@ -2,7 +2,13 @@
 # wondering what else there is; Is there really no better way than
 # encoding all the data as images?
 # Yes, there is, and it is called Shader Storage Buffer Objects, SSBO
-# for short, and "it's just an array of structs" for people like me.
+# for short, and "it's just arrays..." for people like me.
+#
+# Let's do something super-simple: We create a handful of numbers
+# between 0.0 and 1.0, store them in an SSBO, let a shader go
+# `x = 1.0 - x` on them, transfer them back into CPU land, and convert
+# them into Python floats.
+
 
 from array import array
 
@@ -22,6 +28,8 @@ base.accept('escape', base.task_mgr.stop)
 
 num_elements = 2**5
 initial_data_py = [i / (num_elements - 1) for i in range(num_elements)]
+# On the Panda3D side, we call an SSBO a ShaderBuffer, and to store data
+# in it, we first need to convert it into bytes.
 initial_data_bytes = array('f', initial_data_py).tobytes()
 # If you do *not* want to provide initial data, probably because you are
 # generating it on the GPU itself, just pass how many bytes you want to
@@ -33,6 +41,8 @@ initial_data_bytes = array('f', initial_data_py).tobytes()
 # `UH_stream`, `UH_dynamic`, and `UH_static` (from "close to host" to
 # "on VRAM and doesn't get updated from the CPU"), but, well, that's
 # history now.
+# The name, here `DataBuffer`, will be used in the shader to identify
+# the buffer.
 ssbo = ShaderBuffer(
     'DataBuffer',
     initial_data_bytes,
@@ -43,10 +53,20 @@ ssbo = ShaderBuffer(
 shader_source = """#version 430
 layout (local_size_x = 32, local_size_y = 1) in;
 
+// This bit of code is utterly superfluous, as it is just another name
+// for `float`. But while we are here: When you work on structs with
+// multiple fields, and your shader only uses a few of those, then only
+// those will be transferred from memory to the compute units.
 struct Data {
   float value;
 };
 
+// This is the actual SSBO. An SSBO can contain several arrays, and on
+// the last one, you don't have to specify a size, because its size can
+// be inferred from the size of the buffer.
+// `std430` defines the memory format, and is the only currently useful
+// option. Older cards improved performance by padding their memory
+// representations; That is now detrimental.
 layout(std430) buffer DataBuffer {
   Data data[];
 };
@@ -57,6 +77,7 @@ void main() {
 }
 """
 
+# From here on out, you have already seen everything.
 
 # Setting up the shader
 shader = Shader.make_compute(Shader.SL_GLSL, shader_source)
